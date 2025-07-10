0a10fa0db15f035f1c853b8fed97f0e226d1826a
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-10 18:11:01 +0200

    We can save pgn file

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:chess_position_binder/core/read_positions.dart';
 import 'package:chess_position_binder/pages/position_editor.dart';
 import 'package:chessground/chessground.dart';
-import 'package:dartchess/dartchess.dart';
+import 'package:dartchess/dartchess.dart' as chess;
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:file_picker/file_picker.dart';
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
     final pgn = await Navigator.of(context).push<String>(
       MaterialPageRoute(
         builder: (context) {
-          return PositionEditorPage(initialFen: Chess.initial.fen);
+          return PositionEditorPage(initialFen: chess.Chess.initial.fen);
         },
       ),
     );
@@ -42,12 +42,18 @@ class _MyHomePageState extends State<MyHomePage> {
       return;
     }
 
-    await FilePicker.platform.saveFile(
+    final selectedOutput = await FilePicker.platform.saveFile(
       dialogTitle: 'Please select an output file:',
       allowedExtensions: ['pgn'],
       fileName: 'output-file.pgn',
       initialDirectory: _currentDirectory?.path,
     );
+    if (selectedOutput == null) {
+      return;
+    }
+
+    final savedFile = File(selectedOutput);
+    await savedFile.writeAsString(pgn);
   }
 
   @override
@@ -69,11 +75,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     final currentItem = snapshot.data![index];
                     final itemName = currentItem.$1;
                     final itemPgn = currentItem.$2;
-                    final pgnGame = PgnGame.parsePgn(itemPgn);
-                    final position = PgnGame.startingPosition(pgnGame.headers);
-                    final itemOrientation = position.turn == Side.white
-                        ? Side.white
-                        : Side.black;
+                    final pgnGame = chess.PgnGame.parsePgn(itemPgn);
+                    final position = chess.PgnGame.startingPosition(
+                      pgnGame.headers,
+                    );
+                    final itemOrientation = position.turn == chess.Side.white
+                        ? chess.Side.white
+                        : chess.Side.black;
                     return ListTile(
                       leading: Text(itemName),
                       title: StaticChessboard(
