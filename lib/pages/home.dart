8fe40b707480061e857c1deca9e59e856c7cf37d
laurent bernabé <laurent.bernabe@gmail.com>
2025-07-12 19:40:07 +0200

    load content on startup

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,7 +21,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    _setupCurrentDirectory();
+    _setupCurrentDirectory().then((_) => _reloadContent());
   }
 
   Future<void> _reloadContent() async {
