481d1cb08e0427606d9959749ba2ceefe377da75
laurent bernabé <laurent.bernabe@gmail.com>
2026-04-05 01:57:52 +0200

    bump android gradle configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,15 +17,15 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.loloof64.chess_position_binder"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
